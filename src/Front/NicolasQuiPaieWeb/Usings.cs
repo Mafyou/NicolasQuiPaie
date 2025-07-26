6eb9f919ae331afd65e7d297abccf0da4e99ad72
Mafyou <Sata77@gmail.com>
2025-07-26 15:43:36 +0200

    favicon & fix errors & enhance loading time

--- src/Front/NicolasQuiPaieWeb/Usings.cs
+++ src/Front/NicolasQuiPaieWeb/Usings.cs
@@ -1,10 +1,6 @@
 // Global using statements for NicolasQuiPaieWeb project
-
-// System namespaces
-// Third-party libraries
 global using Blazored.LocalStorage;
 global using Microsoft.AspNetCore.Components.Authorization;
-// Microsoft Blazor namespaces
 global using Microsoft.AspNetCore.Components.Web;
 global using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 global using Microsoft.Extensions.DependencyInjection;
@@ -12,7 +8,6 @@ global using Microsoft.Extensions.Logging;
 global using Microsoft.Extensions.Options;
 global using NicolasQuiPaieData.DTOs;
 global using NicolasQuiPaieWeb.Authentication;
-// Project-specific namespaces
 global using NicolasQuiPaieWeb.Components;
 global using NicolasQuiPaieWeb.Configuration;
 global using NicolasQuiPaieWeb.Services;
